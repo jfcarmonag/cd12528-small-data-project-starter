fa6eedb529e7691168f9974adc2abfe0ee49c801
Juan_Felipe <jfcarmonag@gmail.com>
2023-03-21 19:03:48 -0500

    part 1

--- starter_code/part1-transfer/transfer_learning.py
+++ starter_code/part1-transfer/transfer_learning.py
@@ -12,6 +12,9 @@ from TrainModel import train_model
 from TestModel import test_model
 from torchvision import models
 
+from collections import OrderedDict
+
+
 
 # use this mean and sd from torchvision transform documentation
 mean = [0.485, 0.456, 0.406]
@@ -21,7 +24,27 @@ std = [0.229, 0.224, 0.225]
 
 #<<<YOUR CODE HERE>>>
 
-
+data_transforms = {
+    'train': transforms.Compose([
+        transforms.RandomRotation(30),
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(mean, std)
+    ]),
+    'valid': transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean, std)
+    ]),
+    'test': transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean, std)
+    ]),
+}
 
 
 #Set up DataLoaders (train, val, and test)
@@ -32,7 +55,18 @@ num_workers = 4
 
 #hint, create a variable that contains the class_names. You can get them from the ImageFolder
 
+data_dir = 'imagedata-50'
+image_datasets = {
+    'train': datasets.ImageFolder(data_dir + '/train', data_transforms['train']),
+    'valid': datasets.ImageFolder(data_dir + '/val', data_transforms['valid']),
+    'test': datasets.ImageFolder(data_dir + '/test', data_transforms['test']),
+}
 
+train_loader = torch.utils.data.DataLoader(image_datasets['train'], batch_size=64, shuffle=True)
+val_loader = torch.utils.data.DataLoader(image_datasets['valid'], batch_size=64, shuffle=True)
+test_loader = torch.utils.data.DataLoader(image_datasets['test'], batch_size=64, shuffle=True)
+
+class_names = {idx: cl for idx, cl in image_datasets['train'].class_to_idx.items()}
 
 # Using the VGG16 model for transfer learning 
 # 1. Get trained model weights
@@ -40,7 +74,24 @@ num_workers = 4
 # 3. Replace top layer classifier with a classifer for our 3 categories
 
 #<<<YOUR CODE HERE>>>
+model = models.vgg16(pretrained=True)
 
+for param in model.parameters():
+    param.requires_grad = False
+
+classifier = nn.Sequential(OrderedDict([
+                          ('fc1', nn.Linear(25088, 1000)),
+                          ('relu1', nn.ReLU()),
+                          ('dropout1', nn.Dropout(p=0.5)),
+                          ('fc2', nn.Linear(1000, 3)),
+                          ('output', nn.LogSoftmax(dim=1))
+                          ]))
+    
+model.classifier = classifier
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+model.to(device)
 # Train model with these hyperparameters
 # 1. num_epochs 
 # 2. criterion 
@@ -48,13 +99,19 @@ num_workers = 4
 # 4. train_lr_scheduler 
 
 #<<<YOUR CODE HERE>>>
+num_epochs = 2
+criterion = nn.NLLLoss()
+lr=0.001
+optimizer = optim.Adam(model.classifier.parameters(), lr=lr)
+train_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)
+
 
 
 # When you have all the parameters in place, uncomment these to use the functions imported above
-#def main():
-#   trained_model = train_model(model, criterion, optimizer, train_lr_scheduler, train_loader, val_loader, num_epochs=num_epochs)
-#   test_model(test_loader, trained_model, class_names)
+def main():
+   trained_model = train_model(model, criterion, optimizer, train_lr_scheduler, train_loader, val_loader, num_epochs=num_epochs)
+   test_model(test_loader, trained_model, class_names)
 
-#if __name__ == '__main__':
-#    main()
-#    print("done")
+if __name__ == '__main__':
+    main()
+    print("done")
